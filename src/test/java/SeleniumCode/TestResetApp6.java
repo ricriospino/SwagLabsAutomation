0c3501b98b8f429bb40988cbe1f5c7e97e547570
ricriospino <68203094+ricriospino@users.noreply.github.com>
2023-08-11 00:52:56 -0500

    commit 11/08

---
+++ src/test/java/SeleniumCode/TestResetApp6.java
@@ -0,0 +1,64 @@
+package SeleniumCode;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TestResetApp6 extends TestBase {
+	
+	Properties properties = new Properties();
+
+	@Given("^el usuario esta logueado$")
+	public void elUsuarioEstaLogueado() throws Throwable {
+		
+		properties.load(new FileReader("C:\\Users\\PC\\git\\SwagLabsAutomation\\src\\test\\java\\resurces\\config.properties"));
+		
+		driver.findElement(loginPage.getInputUserName()).sendKeys(properties.getProperty("usuario"));			
+	    driver.findElement(loginPage.getInputPassword()).sendKeys(properties.getProperty("contrasena"));
+		driver.findElement(loginPage.getBotonLogin()).submit();  
+		//Thread.sleep(2000);
+	
+	}
+	
+	@Given("^el usuario ha seleccionado al menos un producto en el carrito$")
+	public void elUsuarioHaSeleccionadoAlMenosUnProductoEnElCarrito() throws Throwable {
+		
+		driver.findElement(homePage.getAgregarProductoCaro()).click();
+		//Thread.sleep(2000);
+		driver.findElement(homePage.getAgregarProductoBarato()).click();
+		//Thread.sleep(2000);
+		
+	   
+	}
+
+	@When("^el usuario realiza un reset de la App de compra$")
+	public void elUsuarioRealizaUnResetDeLaAppDeCompra() throws Throwable {
+		
+		driver.findElement(homePage.getMenuBurger()).click();
+		Thread.sleep(1000);
+		driver.findElement(menuPage.getBotonResetApp()).click();
+		//Thread.sleep(2000);
+		
+		
+	
+	}
+
+	@Then("^el carrito de compra debería estar vacío$")
+	public void elCarritoDeCompraDeberíaEstarVacío() throws Throwable {
+		
+		WebElement carrito = driver.findElement(homePage.getCarrito());
+		
+		if(carrito.getText().equals("")) {
+			Assert.assertTrue(true);
+		}else {
+			Assert.assertTrue(false);
+		}
+	}
+
+}
